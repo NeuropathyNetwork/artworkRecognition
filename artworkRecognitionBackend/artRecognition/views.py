a4af76862baed437368c5d77347dcd33ead593db
chara <charaly_cha@outlook.com>
2024-11-24 15:34:16 +0800

    update

--- artworkRecognitionBackend/artRecognition/views.py
+++ artworkRecognitionBackend/artRecognition/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+# from static.utils.crawler import fetch_baike_info
 # Create your views here.
 
 
@@ -7,4 +7,11 @@ def index(request):
     return render(request, "index.html")
 
 def analyze(request):
+    # if request.method == 'POST':
+    #     key_word = request.POST.get('key_word')
+    #     result = fetch_baike_info(key_word)
+    #     return render(request, 'analyze.html', {'result': result})
+    # result = fetch_baike_info('莫奈')
+    # print(result)
+    #return render(request, 'analyze.html', {'result': result})
     return render(request, 'analyze.html')
