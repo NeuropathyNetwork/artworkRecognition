b7c0e818fc15dc1bd52d52e2335f7cf0c294ce51
chara <charaly_cha@outlook.com>
2025-01-05 19:05:38 +0800

    update

--- artworkRecognitionBackend/static/utils/crawler.py
+++ artworkRecognitionBackend/static/utils/crawler.py
@@ -27,42 +27,49 @@ def fetch_baike_info(key_word):
         response = requests.get(url1 + key_word_encoded, headers=headers)
         response.raise_for_status()
         response.encoding = 'utf-8'
+
         soup = BeautifulSoup(response.text, 'html.parser')
+
         # log_dir = 'logs'
         # if not os.path.exists(log_dir):
         #     os.makedirs(log_dir)
         # log_file_path = os.path.join(log_dir, f"{key_word_encoded}.html")
         # with open(log_file_path, 'w', encoding='utf-8') as log_file:
         #     log_file.write(response.text)
+
         meta_description = soup.find('meta', attrs={'name': 'description'})
         if meta_description:
             result["description"] = meta_description.get('content')
 
-        dt_tag = soup.find('dt', class_='basicInfoItem_vbiBk itemName_fOdwv', string='出生日期')
+        dt_tag = soup.find('dt', class_='basicInfoItem_NiU_G itemName_Tk1oH', string='出生日期')
         if dt_tag:
-            dd_tag = dt_tag.find_next_sibling('dd', class_='basicInfoItem_vbiBk itemValue_JaQOj')
+            dd_tag = dt_tag.find_next_sibling('dd', class_='basicInfoItem_NiU_G itemValue_IEtvZ')
             if dd_tag:
-                span_tag = dd_tag.find('span', class_='text_B_eob')
+                span_tag = dd_tag.find('span', class_='text_QZdcK')
                 if span_tag:
                     result["birth_date"] = span_tag.get_text(strip=True)
 
-        dt_tag = soup.find('dt', class_='basicInfoItem_vbiBk itemName_fOdwv', string='逝世日期')
+        dt_tag = soup.find('dt', class_='basicInfoItem_NiU_G itemName_Tk1oH', string='逝世日期')
         if dt_tag:
-            dd_tag = dt_tag.find_next_sibling('dd', class_='basicInfoItem_vbiBk itemValue_JaQOj')
+            dd_tag = dt_tag.find_next_sibling('dd', class_='basicInfoItem_NiU_G itemValue_IEtvZ')
             if dd_tag:
-                span_tag = dd_tag.find('span', class_='text_B_eob')
+                span_tag = dd_tag.find('span', class_='text_QZdcK')
                 if span_tag:
                     result["death_date"] = span_tag.get_text(strip=True)
 
         # 获取代表作品
-        dt_tag = soup.find('dt', class_='basicInfoItem_vbiBk itemName_fOdwv', string='代表作品')
+        dt_tag = soup.find('dt', class_='basicInfoItem_NiU_G itemName_Tk1oH', string='代表作品')
         if dt_tag:
-            dd_tag = dt_tag.find_next_sibling('dd', class_='basicInfoItem_vbiBk itemValue_JaQOj')
+            dd_tag = dt_tag.find_next_sibling('dd', class_='basicInfoItem_NiU_G itemValue_IEtvZ')
             if dd_tag:
-                span_tags = dd_tag.find_all('span', class_='text_B_eob')
+                span_tags = dd_tag.find_all('span', class_='text_QZdcK')
                 for span in span_tags:
                     if span.get_text(strip=True) != "，":
-                        result["works"].append(span.get_text(strip=True))
+                        text = span.get_text(strip=True)
+                        if text.endswith("等"):
+                            text = text[:-1]
+                        result["works"].append(text)
+
 
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP错误: {http_err}")
@@ -76,7 +83,7 @@ def fetch_baike_info(key_word):
         print(f"其他错误: {e}")
 
     return result
-
+#
 # if __name__ == '__main__':
 #     artist = '梵高'
 #     result = fetch_baike_info(artist)
